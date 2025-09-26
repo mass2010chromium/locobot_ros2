c63b80e31673cd04d5ab14fd57be9b68f9b3bcdb
locobot-SER8 <locobot-SER8>
2025-09-26 10:44:41 -0400

    Add script for nav testing

---
+++ locobot_navigation/scripts/nav_debug_com.py
@@ -0,0 +1,59 @@
+from geometry_msgs.msg import PoseStamped
+from nav2_simple_commander.robot_navigator import BasicNavigator, TaskResult
+
+import rclpy
+from rclpy.duration import Duration
+
+
+def main() -> None:
+    rclpy.init()
+    nav = BasicNavigator()
+    
+    ####this part isn't completely necessary. however it's useful when running the script more than once
+    init_pose = PoseStamped() #default (x, y, z) pos = 0.0
+    init_pose.header.frame_id = 'map'
+    init_pose.header.stamp = nav.get_clock().now().to_msg()
+    init_pose.pose.position.x = 0.0
+    init_pose.pose.position.y = 0.0
+    init_pose.pose.position.z = 0.0
+    init_pose.pose.orientation.w = 1.0 #other two quaternion values set to 0 by default
+    init_pose.pose.orientation.z = 0.0
+    nav.setInitialPose(init_pose)
+    ####
+
+    goal_pose = PoseStamped()
+    goal_pose.header.frame_id = 'map'
+    goal_pose.header.stamp = nav.get_clock().now().to_msg()
+    goal_pose.pose.position.x = 0.0
+    goal_pose.pose.position.y = 0.0
+
+    path = nav.getPath(init_pose, goal_pose) #to see if a valid path exists
+    go_to_pose_task = nav.goToPose(goal_pose)
+
+    i = 0
+    start_time = nav.get_clock().now()
+    while not nav.isTaskComplete(task = go_to_pose_task):
+        i += 1
+        feedback = nav.getFeedback(task = go_to_pose_task)
+        if feedback:
+            print('ETA: ' + '{:.0f}'.format(Duration.from_msg(feedback.estimated_time_remaining).nanoseconds/1e9)
+                  + ' secs')
+            
+            if Duration.from_msg(feedback.navigation_time) > Duration(seconds = 90):
+                print("Took too long, cancelling task...")
+                nav.cancelTask()
+    end_time = nav.get_clock().now()
+    duration = end_time - start_time
+    result = nav.getResult()
+    if result == TaskResult.SUCCEEDED:
+        print(f'Goal succeeded in {duration.nanoseconds * 1e-9:.2f} seconds')
+    elif result == TaskResult.FAILED:
+        print('Goal failed!')
+    else:
+        print('Goal has invalid return status!')
+
+    nav.lifeclycleShutdown()
+    exit(0)
+
+if __name__ == '__main__':
+    main()
