8219a1e9ba52472e43344e4658276a5d14af60ad
locobot-SER8 <locobot-SER8>
2025-10-16 00:32:33 -0400

    Egocircle depth for hole detection
    Somehow, the depth resolution I had to use for hole detection
    is very bad...
    In retrospect, I should use two different scaling laws for
    normal (robot-height) obstacles (whose resolution scales as native
    depth resolution) and "hole-type" obstacles (whose resolution
    scales as the native angular resolution).
    TODO: See if justin has something for this in c++

--- locobot_navigation/locobot_navigation/ego_circle.py
+++ locobot_navigation/locobot_navigation/ego_circle.py
@@ -19,6 +19,81 @@ class EgoCircle:
         self.point_stamps = []
         self.latest_stamp = 0
 
+    def add_depth_cloud_hazards(self, ego_pose, local_points,
+                                ground_z, robot_height, tol=0.02,
+                                theta_step=0.005, r_step=0.01,
+                                r_min=0.3, r_max=None,
+                                priority=0, stamp=None):
+        # Points are expected in robot-local coordinates
+
+        if r_max is None:
+            r_max = self.max_range
+
+        # Assuming no wraparound (forward facing ish camera)
+        zs = local_points[:, 2]
+        angles = np.arctan2(local_points[:, 1], local_points[:, 0])
+        rs = np.linalg.norm(local_points[:, :2], axis=1)
+        zs = zs[rs > r_min]
+        angles = angles[rs > r_min]
+        rs = rs[rs > r_min]
+
+        # Inverse map for resolution
+        # https://www.desmos.com/calculator/yohjwu7osz
+        r_idx = np.floor((r_min/r_step) * (1 - (r_min / rs))).astype(int)
+        n_r = np.ceil(r_min/r_step) + 1
+
+        theta_min = np.min(angles)
+        theta_max = np.max(angles)
+        n_r = int(np.ceil((r_max) / r_step)) + 1
+        n_theta = int(np.ceil((theta_max - theta_min) / theta_step)) + 1
+        theta_step = (theta_max - theta_min) / (n_theta - 1)
+
+        ground_min = ground_z - tol
+        ground_max = ground_z + tol
+        on_ground = np.logical_and(zs > ground_min, zs < ground_max)
+        collides = np.logical_and(zs > ground_max, zs < robot_height),
+
+        theta_idx = ((angles - theta_min) / theta_step).astype(int)
+
+        ground_bins = np.zeros((n_theta, n_r), dtype=int)
+        ground_bins[theta_idx[on_ground], r_idx[on_ground]] = 1
+        obstacle_bins = np.zeros((n_theta, n_r), dtype=int)
+        obstacle_bins[theta_idx[collides], r_idx[collides]] = 3
+        grid_map = np.maximum(ground_bins, obstacle_bins)
+        
+        # Array will be 1 on transitions from ground to unknown.
+        res = []
+        cliff_detect = grid_map[:, :-1] - grid_map[:, 1:]
+        l = len(cliff_detect) // 6
+        cliff_detect[:l] = 0
+        cliff_detect[-l:] = 0
+        thetas = np.linspace(theta_min, theta_max, n_theta)
+        rays = np.array([
+            np.cos(thetas),
+            np.sin(thetas),
+            np.zeros_like(thetas)]
+        ).T
+
+        # Drop the last r step so indices line up
+        for obs_ray, cliff_ray, ray in zip(grid_map[:, :-1], cliff_detect, rays):
+            hazards = np.argwhere(np.logical_or(obs_ray >= 3, cliff_ray == 1))
+            if len(hazards) > 0:
+                # Inverting the inverse map
+                r = r_min / (1 - (hazards[0] * (r_step/r_min)))
+                if r_min < r and r < r_max:
+                    res.append(r * ray)
+        if len(res) == 0:
+            return
+        res = np.array(res)
+        res[:, 2] = 1
+
+        # global points are at ego_pose @ local_points
+        pose_2d = np.zeros((3, 3))
+        pose_2d[:2, :2] = ego_pose[:2, :2]
+        pose_2d[:2, 2] = ego_pose[:2, 3]
+        pose_2d[2, 2] = 1
+        self.add_scan_points(res @ pose_2d.T, priority=priority, stamp=stamp)
+
     def add_scan_points(self, points, priority=0, stamp=None):
         """
         Add a scan measurement from a 3d point cloud.
@@ -174,13 +249,13 @@ if __name__ == '__main__':
     node = Node("egocircle")
 
     laser_frame = "laser_upright"
-    base_frame = "base_link"
+    base_frame = "base_footprint"
     map_frame = "map"
     camera_frame = "camera_frame"
     transforms = {
         "laser_pose": (map_frame, laser_frame),
         "pose": (map_frame, base_frame),
-        "camera_transform": (map_frame, camera_frame)
+        "camera_transform": (base_frame, camera_frame)
     }
 
     from motionlib import so3, se3
@@ -197,9 +272,6 @@ if __name__ == '__main__':
     )
 
     pub = node.create_publisher(LaserScan, '/fused_scan', 10)
-    crop_min = np.array([-np.inf, -np.inf, 0.1])
-    crop_max = np.array([np.inf, np.inf, 1.0])
-    crop_box = AxisAlignedBoundingBox(crop_min, crop_max)
 
     # from datasource.depth_anything import DepthPredictor
     # import torch
@@ -226,10 +298,14 @@ if __name__ == '__main__':
         robot_pose = np.array(se3.homogeneous(frames['pose']))
         cam_pose = np.array(se3.homogeneous(frames['camera_transform']))
         accumulator.add_scan(scan_pose, scan_arr, angles, priority=0)
-        o3d_pc = form_point_cloud(color_image=frames['color'], depth_image=rs_source.depth_to_meters(frames['depth']),
-                                  intrinsics_mat=rs_source.intrinsics, pose=cam_pose).crop(crop_box)
+        o3d_pc = form_point_cloud(depth_image=rs_source.depth_to_meters(frames['depth']),
+                                  intrinsics_mat=rs_source.intrinsics, pose=cam_pose)
         points = np.asanyarray(o3d_pc.points)
-        #accumulator.add_scan_points(points, priority=1)
+        accumulator.add_depth_cloud_hazards(robot_pose, points,
+                                0, 1, tol=0.10,
+                                theta_step=0.01, r_step=0.01,
+                                r_min=0.4, r_max=1.5,
+                                priority=1)
 
         scan_sim, points = accumulator.simulate_scan(robot_pose, prune=True)
 
